2f6e51e97bef53ee2daa4c496dea52dce270aa35
Mickael <mickaelbeites@MacBook-Air-de-Mickael.local>
2019-04-28 19:06:05 +0200

    no photo

---
+++ contact.php
@@ -0,0 +1,107 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/*
+THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
+*/
+
+require 'vendor/autoload.php';
+
+/*
+*  CONFIGURE EVERYTHING HERE
+*/
+// an email address that will be in the From field of the email.
+$fromEmail = 'demo@domain.com';
+$fromName = 'Demo contact form';
+
+// an email address that will receive the email with the output of the form
+$sendToEmail = 'demo@domain.com';
+$sendToName = 'Demo contact form';
+
+// subject of the email
+$subject = 'New message from contact form';
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'firstname' => 'Firstname', 'email' => 'Email', 'subjet' => 'Subject', 'message' => 'Message');
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+$emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
+$emailTextHtml .= "<table>";
+
+foreach ($_POST as $key => $value) {
+    // If the field exists in the $fields array, include it in the email
+    if (isset($fields[$key])) {
+        $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+    }
+}
+
+$emailTextHtml .= "</table><hr>";
+$emailTextHtml .= "<p>Have a nice day,<br>Mickael,<br>Beites</p>";
+
+$mail = new PHPMailer;
+
+$mail->setFrom($fromEmail, $fromName);
+$mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
+$mail->addReplyTo($from);
+
+$mail->isHTML(true);
+
+$mail->Subject = $subject;
+$mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
+
+if (!$mail->send()) {
+    throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+}
+
+$smtpHost = 'smtp.domain.com';
+$smtpUsername = 'hello@domain.com';
+$smtpPassword = 'PASSWORD';
+
+$mail = new PHPMailer;
+
+... - set from, recipient, message...
+
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+$mail->Host = gethostbyname($smtpHost);
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+//We have configured this variable in the config section
+$mail->Username = $smtpHost;
+
+//Password to use for SMTP authentication
+//We have configured this variable in the config section
+$mail->Password = $smtpPassword;
+
+
+if(!$mail->send()) {
+    throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+}
